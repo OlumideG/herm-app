021f26b05a9994c394bf149e4f797b6ff5ce431a
OlumideG <olumidegbenjo@gmail.com>
2020-05-24 12:29:43 +0100

    initial commit

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Text } from 'herm';
+import Layout from '../components/Layout';
+
+
+function Index() {
+  return (
+    <Layout>
+      <Text fontSize="32px">Hello, Herm</Text>
+    </Layout>
+  );
+}
+export default Index;
